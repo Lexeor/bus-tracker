46838c95893412841c15943f5c13d8a31400ef06
Alexander Tarasov <lexeor.ross@gmail.com>
2025-10-30 12:42:22 +0100

    move constants to a separate file

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// localStorage key for visible routes
+export const VISIBLE_ROUTES_KEY = 'busapp-visible-routes';
+// Temporary center at Herceg Novi
+export const defaultCenter: [number, number] = [42.453, 18.531];
+
+export const DISCLAIMER_STORAGE_KEY = 'disclaimer_seen';
